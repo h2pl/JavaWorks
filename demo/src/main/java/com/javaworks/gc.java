40d5741ef6b6480168248c8b8aed453c9ef2efdb
penglong.huang <penglong.huang@mihoyo.com>
2022-09-10 09:06:29 +0800

    manual gc

---
+++ demo/src/main/java/com/javaworks/gc.java
@@ -0,0 +1,33 @@
+package com.javaworks;
+
+/**
+ * Created by Intellij IDEA.
+ * User:  penglong.huang
+ * Date:  2022/9/10
+ */
+public class gc {
+
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.aa);
+        a = null;
+        System.out.println(a);
+        System.gc();
+        while (true) {
+
+        }
+    }
+
+    private void gc() {
+    }
+
+    static class A extends Object {
+        private String aa = "aa";
+
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println("gc ok");
+        }
+    }
+
+}
